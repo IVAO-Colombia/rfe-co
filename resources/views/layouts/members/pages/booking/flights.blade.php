43c0988fbef38c3dd7938e628389c1cc047a7d41
Edgardo Alvarez <edgardo@livedesign.org>
2022-09-17 14:55:25 -0500

    “update”

--- resources/views/layouts/members/pages/booking/flights.blade.php
+++ resources/views/layouts/members/pages/booking/flights.blade.php
@@ -69,6 +69,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-popRpmFF9JQgExhfw5tZT4I9/CI5e2QcuUZPOVXb1m7qUmeR2b50u+YFEYe1wgzy" crossorigin="anonymous">
     </script>
+
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
+        integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
